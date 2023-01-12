b173f798bc3ef80f0c72090e996e70fb9160492f
AF1QUE <afique.dev@gmail.com>
2023-01-12 06:06:29 +0600

    Finished building voting app

--- server/index.js
+++ server/index.js
@@ -10,33 +10,36 @@ app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
 });
 
 app.get("/poll", async (req, res) => {
-    let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
-    const totalVotes = Object.values(data).reduce((total, n) => total += n, 0);
+  let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
 
-    data = Object.entries(data).map(([label, votes]) => {
-        return {
-            label,
-            percentage: (((100 * votes) / totalVotes) || 0).toFixed(0)
-        }
-    });
+  const totalVotes = Object.values(data).reduce((total, n) => {
+    return (total += n);
+  }, 0);
 
-    res.json(data);
+  data = Object.entries(data).map(([label, votes]) => {
+    return {
+      label,
+      percentage: ((100 * votes) / totalVotes || 0).toFixed(0),
+    };
+  });
+
+  res.json(data);
 });
 
 app.post("/poll", async (req, res) => {
-    const data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+  const data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
 
-    data[req.body.add]++;
+  console.log(data);
+  data[req.body.add]++;
 
-    await fs.writeFile(dataFile, JSON.stringify(data));
+  await fs.writeFile(dataFile, JSON.stringify(data, null, 2));
 
-    res.end();
+  res.json(data);
 });
 
-app.listen(3000, () => console.log("Server is running..."));
+app.listen(4000, () => console.log("Server is running..."));
